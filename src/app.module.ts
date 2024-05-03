afe610b9b72b33714c34872b36d21327ab0503e5
Fidelis Adewole <adewoyeadedayo10@gmail.com>
2024-05-03 09:38:14 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MONGODB_URI } from './config/env';
+const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.cwzz5uc.mongodb.net/${MONGODB_DATABASE}`;
 @Module({
   imports: [AuthModule, MongooseModule.forRoot(MONGODB_URI)],
   controllers: [],
