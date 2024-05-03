46667cc530edcf7918ec2cc2b2187a19602c3b05
Fidelis Adewole <adewoyeadedayo10@gmail.com>
2024-05-03 09:39:29 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
-const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.cwzz5uc.mongodb.net/${MONGODB_DATABASE}`;
+const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.cwzz5uc.mongodb.net/${MONGODB_DB}`;
 @Module({
   imports: [AuthModule, MongooseModule.forRoot(MONGODB_URI)],
   controllers: [],
