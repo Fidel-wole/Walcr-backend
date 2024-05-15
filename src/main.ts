83419eba0c864d74faba67d0918e4491e4b46bae
Adewole Adewoye <adewoyeadedayo10@gmail.com>
2024-05-15 16:35:06 +0100

    feat: Add card

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+// eslint-disable-next-line prettier/prettier
+const port = process.env.PORT 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port || 3000);
 }
 bootstrap();
